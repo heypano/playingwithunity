176a7116e5884abc13686a9e0c0fbb4a0695ca3a
Pano Papadatos <pano.papadatos@publicrelay.com>
2016-09-05 18:19:19 -0400

    First experiment with Roll a Ball

---
+++ Roll a Ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CameraController : MonoBehaviour {
+	public GameObject player;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;	
+	}
+
+	// Follow Cameras, procedural animation and gathering last known states
+	// guaranteed to run after everything has been processed in update, we know absolutely the player has moved this frame
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
